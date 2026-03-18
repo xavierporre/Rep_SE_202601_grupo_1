5ce1c6d97af940131a3dfc986271e064a01dd947
xavierporre <128742210+xavierporre@users.noreply.github.com>
2026-03-18 14:35:48 -0300

    Update greeting message in hello_world.cpp

--- Lab_1/Ejercicio_1/hello_world.cpp
+++ Lab_1/Ejercicio_1/hello_world.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, Xavier Porre!" << std::endl;
+    std::cout << "Hello, Xavier Porre, Martin Feres, Vicente Abalos!" << std::endl;
     return 0;
 }
